05c26934c6ec79c603ab0f724d2616c4b04065bd
Logan Prasse <lrprasse@mail.usf.edu>
2019-09-12 23:09:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace IterationsDeliverable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Please enter an integer between 1 and 100: ");
+                string input = Console.ReadLine();
+                int value = int.Parse(input);
+
+                if (value > 0 && value < 101)
+                {
+                    for (int i = 1; i <= value; i++)
+                    {
+                        Console.WriteLine("You have entered " + value.ToString() + "." +
+                            " This is the current integer value in the loop: " + i.ToString());
+                        Console.Beep();
+                        System.Threading.Thread.Sleep(500);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The integer you entered is not between 1 and 100.");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Something went wrong! Please restart the program and" +
+                    "enter a valid integer.");
+            }
+        }
+    }
+}
